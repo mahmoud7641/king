eeefa6dbf755b416d525e396742edb2d1aa04316
mahmoud7641 <152595992+mahmoud7641@users.noreply.github.com>
2024-01-24 10:22:53 +0200

    Update speedtest.py

--- AlexaMusic/plugins/tools/speedtest.py
+++ AlexaMusic/plugins/tools/speedtest.py
@@ -39,7 +39,7 @@ def testspeed(m):
 
 @app.on_message(filters.command(SPEEDTEST_COMMAND) & SUDOERS)
 async def speedtest_function(client, message):
-    m = await message.reply_text("يحاول ان يتحقق من سرعة التحميل و الرفع")")
+    m = await message.reply_text("يحاول ان يتحقق من سرعة التحميل و الرفع")
     loop = asyncio.get_event_loop()
     result = await loop.run_in_executor(None, testspeed, m)
     output = f"""**نتائج الاختبار**

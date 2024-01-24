10a5e13d926ba1053ce31ac8fe999ff48f4c2152
mahmoud7641 <152595992+mahmoud7641@users.noreply.github.com>
2024-01-24 11:37:08 +0200

    Update help.py

--- AlexaMusic/utils/inline/help.py
+++ AlexaMusic/utils/inline/help.py
@@ -44,4 +44,5 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_4"],
                     callback_data="help_callback hb4"
                 ),
-            ]
+            ],
+       ]
